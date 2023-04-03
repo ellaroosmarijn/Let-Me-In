484a3310106aaacede1e10e584da94d62b30af71
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-04 11:46:04 +1200

    setup db function to get img by uploder id

--- server/db/uploads.ts
+++ server/db/uploads.ts
@@ -1,3 +1,15 @@
 import connection from "./connection";
-import {Images} from '../../models/'
+import { Image } from '../../models/image'
+
+
+export function uploads(uploderId :string, db = connection){
+return db('images').where('uploderId',uploderId).select(
+  'id',
+  'uploader_id as uploaderId',
+  'name',
+  'description',
+  'image_url as imageUrl'
+)
+
+}
 
