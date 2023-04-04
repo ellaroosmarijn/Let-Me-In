4c86dbece6b9e0f8eac1292ae0c657285b4ca4ba
keanu-pere <83375977+Keanu-Pere@users.noreply.github.com>
2023-04-04 12:19:50 +1200

    testing db functions done

---
+++ server/db/__tests__/uploads.test.ts
@@ -0,0 +1,46 @@
+import connection from "../connection";
+import { uploads } from "../uploads";
+
+beforeAll(() => {
+  return connection.migrate.latest()
+})
+
+beforeEach(() => {
+  return connection.seed.run()
+})
+
+afterAll(() => {
+  return connection.destroy()
+})
+
+describe('getUserImage', () => {
+  it('get the user uploaded images from the database', async () => {
+    
+   // Arrange
+  //  expect.assertions(12)
+  
+  // Act
+  // first we get the user imges Uploads('1' || "2")
+  const user1 = await uploads('1')
+  const user2 = await uploads('2')
+
+  console.log(user1)
+  // Assert
+  // we can check for length 
+  expect(user1).toHaveLength(2)
+  expect(user2).toHaveLength(2)
+
+
+ 
+ 
+ 
+ 
+ 
+  }) 
+
+})
+
+
+
+
+
--- server/db/uploads.ts
+++ server/db/uploads.ts
@@ -1,15 +1,14 @@
-import connection from "./connection";
+import connection from './connection'
 import { Image } from '../../models/image'
 
-
-export function uploads(uploderId :string, db = connection){
-return db('images').where('uploderId',uploderId).select(
-  'id',
-  'uploader_id as uploaderId',
-  'name',
-  'description',
-  'image_url as imageUrl'
-)
-
+export function uploads(uploderId: string, db = connection):  Promise<Image[]>{
+  return db('images')
+    .where('uploader_id', uploderId)
+    .select(
+      'id',
+      'uploader_id as uploaderId',
+      'name',
+      'description',
+      'image_url as imageUrl'
+    )
 }
-
