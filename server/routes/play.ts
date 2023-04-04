1c4cdc2ee6680e4c21a08c02997bc751a368a113
elisa-bertoni <89889937+elisabertoni@users.noreply.github.com>
2023-04-04 15:34:01 +1200

    add checkJWT middleware

--- server/routes/play.ts
+++ server/routes/play.ts
@@ -1,17 +1,26 @@
 import { Router } from 'express'
 import { getWinningImage } from '../db/play'
+import checkJwt, { JwtRequest } from '../auth0'
 
 const router = Router()
 
 // GET '/api/v1/play'
-router.get('/', async (req, res) => {
+router.get('/', checkJwt, async (req, res) => {
+  // const auth0Id = req.auth?.sub
+  //   if (!auth0Id) {
+  //     console.error('No auth0Id')
+  //     return res.status(401).send('Unauthorized')
+  //   }
+    
   try {
     const winningImage = await getWinningImage()
+    
+
     res.json(winningImage)
   } catch (error) {
     console.log(error)
     res.status(500).json({
-      error: 'There was an error trying to get the images',
+      error: 'There was an error trying to get the image',
     })
   }
 })
