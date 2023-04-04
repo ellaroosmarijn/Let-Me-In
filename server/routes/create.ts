d532ca8859d319bab80347965832af725d0d0428
Reuben Hawthorne-Jensen <103969792+reubenj95@users.noreply.github.com>
2023-04-04 12:03:25 +1200

    Add route for adding new image

--- server/routes/create.ts
+++ server/routes/create.ts
@@ -1,5 +1,24 @@
 import { Router } from 'express'
+import checkJwt from '../auth0'
+import { JwtRequest } from '../auth0'
+import db from '../db/create'
 
 const router = Router()
+
+router.post('/', checkJwt, async (req: JwtRequest, res) => {
+  try {
+    const auth0Id = req.auth?.sub
+    if (auth0Id) {
+      const response = db.addImage(req.body, auth0Id)
+      return res.json(response)
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      return res.send(err.message)
+    } else {
+      return res.send('An unknown error has occurred')
+    }
+  }
+})
 
 export default router
