dd7059e6c3f06f41a3fc207fc2cc8bbbfde577fa
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-05 10:50:47 +1200

    initial setup of componants, without the JWT

--- client/components/Uploads.tsx
+++ client/components/Uploads.tsx
@@ -1,7 +1,30 @@
+import { useEffect } from 'react'
+import { fetchUploads } from '../actions/uploads'
+import { useAppSelector, useAppDispatch } from '../hooks'
+
 export default function Uploads() {
+  const uploads = useAppSelector((state) => state.uploads)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchUploads()), []
+  })
+
   return (
     <>
       <h1>UPLOADS PAGE :D</h1>
+      <div>
+        <ul>
+          {uploads.data?.map((image) => {
+            return (
+              <li key={image.id}>
+                <h2>{image.name}</h2>
+                <img src={image.imageUrl} alt={image.description} />
+              </li>
+            )
+          })}
+        </ul>
+      </div>
     </>
   )
 }
--- client/reducers/index.ts
+++ client/reducers/index.ts
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import homeReducer from './home'
 import playReducer from './play'
+import uploadReducer from './uploads'
 
 export default combineReducers({
   home: homeReducer,
   play: playReducer,
+  uploads: uploadReducer,
 })
