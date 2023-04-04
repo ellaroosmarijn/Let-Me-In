dada963906a009e6a791d7fe3cbdba2a1738d03c
Daniel-Petrie <97697409+Daniel-Petrie@users.noreply.github.com>
2023-04-04 16:24:54 +1200

    tested play route

--- server/routes/play.ts
+++ server/routes/play.ts
@@ -1,21 +1,13 @@
 import { Router } from 'express'
 import { getWinningImage } from '../db/play'
-import checkJwt, { JwtRequest } from '../auth0'
+import checkJwt from '../auth0'
 
 const router = Router()
 
 // GET '/api/v1/play'
 router.get('/', checkJwt, async (req, res) => {
-  // const auth0Id = req.auth?.sub
-  //   if (!auth0Id) {
-  //     console.error('No auth0Id')
-  //     return res.status(401).send('Unauthorized')
-  //   }
-    
   try {
     const winningImage = await getWinningImage()
-    
-
     res.json(winningImage)
   } catch (error) {
     console.log(error)
