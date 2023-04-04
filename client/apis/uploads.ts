93e70ff87a81aa0c4bb9c672addaed285aec6a4b
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-05 09:44:08 +1200

    apiClient function created

--- client/apis/uploads.ts
+++ client/apis/uploads.ts
@@ -1,1 +1,11 @@
 import request from 'superagent'
+import type {Image} from '../../models/image'
+
+
+const url ='/api/v1/uploads'
+
+export async function getUploads(): Promise<Image[]> {
+  const response = await request.get(url)
+  console.log(response.body)
+  return response.body
+}
