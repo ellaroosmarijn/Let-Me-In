b2e99425875edfe607b0b67240bde80a44af9f4e
Reuben Hawthorne-Jensen <103969792+reubenj95@users.noreply.github.com>
2023-04-05 16:45:27 +1200

    Add tests for component

--- client/components/__tests__/Create.test.tsx
+++ client/components/__tests__/Create.test.tsx
@@ -1,3 +1,51 @@
-describe('something', () => {
-  it.todo('will test a thing')
+import nock from 'nock'
+import { MemoryRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { screen, render, within } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+
+import App from '../App'
+import { initialiseStore } from '../../store'
+
+describe('<Create />', () => {
+  it('should have heading "create your own entry"', async () => {
+    render(
+      <MemoryRouter initialEntries={['/Create']}>
+        <Provider store={initialiseStore()}>
+          <App />
+        </Provider>
+      </MemoryRouter>
+    )
+
+    const header = await screen.findByRole('heading')
+    expect(header).toHaveTextContent('Create your own entry')
+  })
+  it('should have 2 textbox for name and description', async () => {
+    render(
+      <MemoryRouter initialEntries={['/Create']}>
+        <Provider store={initialiseStore()}>
+          <App />
+        </Provider>
+      </MemoryRouter>
+    )
+
+    const inputItems = await screen.findAllByRole('textbox')
+    expect(inputItems).toHaveLength(2)
+  })
+  it('should have submit button', async () => {
+    render(
+      <MemoryRouter initialEntries={['/Create']}>
+        <Provider store={initialiseStore()}>
+          <App />
+        </Provider>
+      </MemoryRouter>
+    )
+
+    const buttonItem = await screen.findByRole('button', { name: /Upload/i })
+    expect(buttonItem).toHaveLength(1)
+  })
+  it.todo(
+    'if any of the value of 3 inputs is null, the submit button should be disabled'
+  )
 })
