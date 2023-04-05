075879fa6a2768427b481f12be2fdd63226d25d2
Raghav Sidharth <121782551+Raghav-Sidharth@users.noreply.github.com>
2023-04-05 14:37:05 +1200

    Adjusted test assertions for router function - removed unneeded comments - adjusted router to only send status 401

--- server/db/__tests__/results.test.ts
+++ server/db/__tests__/results.test.ts
@@ -17,20 +17,19 @@ afterAll(() => {
 describe('getResults', () => {
   it('gets the results from the results table in the database.', async () => {
     // Arrange
-
     expect.assertions(5)
 
     // Act
     const results = await getResults('1')
 
     // Assert
-    // Since the getResults() database function gets a random image
-    // we can only assert that the Results has the correct properties
-
+    console.log(results)
     expect(results[2].id).toBe(3)
     expect(results[1].auth0Id).toBe('1')
-    expect(results[0].imageId).toBeDefined()
+    expect(results[2].imageId).toBe(3)
     expect(results).toHaveLength(3)
-    expect(results[0].imageUrl).toBeDefined()
+    expect(results[0].imageUrl).toBe(
+      'https://media.tenor.com/bHGUqVIKzhoAAAAC/let-me-in-eric-andre.gif'
+    )
   })
 })
