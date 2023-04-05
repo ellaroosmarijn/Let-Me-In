4b1275bf36642021f5c03ff0a39c01e3ba2728a3
David Gutierrez <114845085+Om-GG@users.noreply.github.com>
2023-04-05 12:14:33 +1200

    Creates the results reducer, imports it to the reducers combiner, now it is in the store. Done with Lo and Chad.

--- client/reducers/index.ts
+++ client/reducers/index.ts
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux'
 
 import homeReducer from './home'
 import playReducer from './play'
+import resultsReducer from './results'
 
 export default combineReducers({
   home: homeReducer,
   play: playReducer,
+  results: resultsReducer,
 })
