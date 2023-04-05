da6e30de01986d1413bd620b5318bcf2dd11f525
elisa-bertoni <89889937+elisabertoni@users.noreply.github.com>
2023-04-05 15:43:46 +1200

    dipslay all 9 images"

--- client/components/Play.tsx
+++ client/components/Play.tsx
@@ -1,23 +1,36 @@
 import { useState, useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../hooks'
 import { useAuth0 } from '@auth0/auth0-react'
+import { Image } from '../../models/image'
 
 import { fetchPlayContent } from '../actions/play'
 
 export default function Play() {
+
+  const [arr, setArr] = useState<Image[]>([
+    {id: 1001, isWinning:false, imageUrl:'images/wrong1.gif' , uploaderId: "developers",  name:"placeholder-image", description: "placeholder-description" },
+    {id: 1002, isWinning:false, imageUrl:'images/wrong2.gif', uploaderId: "developers",  name:"placeholder-image", description: "placeholder-description" },
+    {id: 1003, isWinning:false, imageUrl:'images/wrong3.gif', uploaderId: "developers", name:"placeholder-image", description: "placeholder-description"  },
+    {id: 1004, isWinning:false, imageUrl:'images/wrong4.gif', uploaderId: "developers", name:"placeholder-image", description: "placeholder-description"  },
+    {id: 1005, isWinning:false, imageUrl:'images/wrong5.gif', uploaderId: "developers", name:"placeholder-image", description: "placeholder-description"  },
+    {id: 1006, isWinning:false, imageUrl:'images/wrong6.gif', uploaderId: "developers", name:"placeholder-image", description: "placeholder-description"  },
+    {id: 1007, isWinning:false, imageUrl:'images/wrong7.gif', uploaderId: "developers", name:"placeholder-image", description: "placeholder-description" },
+    {id: 1008, isWinning:false, imageUrl:'images/wrong8.gif', uploaderId: "developers",  name:"placeholder-image", description: "placeholder-description" },
+  ])
+
   const { loading, error, data } = useAppSelector((state) => state.play)
-
   const dispatch = useAppDispatch()
-
   const { getAccessTokenSilently } = useAuth0()
-
-  useEffect(() => {
+  const dataArr = [...arr, data]
+ 
+   useEffect(() => {
     async function dispatchFetchPlayContent() {
       const token = await getAccessTokenSilently()
       dispatch(fetchPlayContent(token))
+       
     }
 
-    dispatchFetchPlayContent()
+   dispatchFetchPlayContent()
   }, [dispatch, getAccessTokenSilently])
 
   return (
@@ -25,7 +38,9 @@ export default function Play() {
       <h1>Catchy Header</h1>
       {error && <p>{error}</p>}
       {loading && <img src="/loading.gif" alt="loading spinner" />}
-      <img src={data?.imageUrl} alt="" />
+      {dataArr.map((image)=>{
+        return(<img key={image?.id} src={image?.imageUrl} alt="facedown cards" />) 
+      })}
     </div>
   )
 }
