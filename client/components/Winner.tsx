df48dce4c905081dae3104ce33fc934c197bbdb0
joseph-ford10 <122236639+joseph-ford10@users.noreply.github.com>
2023-04-06 12:15:19 +1200

    Add logic for Winner component

--- client/components/Winner.tsx
+++ client/components/Winner.tsx
@@ -1,7 +1,36 @@
+import { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from '../hooks'
+import { Navigate } from 'react-router-dom'
+
+import { postResultAdd } from '../actions/winner'
+
 export default function Winner() {
+  const dispatch = useAppDispatch()
+  const winStatus = useAppSelector((state) => state.play)
+  console.log(winStatus)
+
+  useEffect(() => {
+    async function ifImage() {
+      return dispatch(postResultAdd(winStatus.data.image))
+    }
+
+    function ifNoImage() {
+      return <Navigate to="/play" />
+    }
+    if (winStatus.data.image.id) {
+      ifImage()
+    } else {
+      ifNoImage()
+    }
+  }, [dispatch, winStatus.data.image])
+
   return (
-    <>
-      <h1>WINNER PAGE :D</h1>
-    </>
+    <div className="display-image">
+      <h1>YOU ARE A WINNER</h1>
+      <img
+        src={winStatus.data.image.imageUrl}
+        alt={winStatus.data.image.description}
+      />
+    </div>
   )
 }
