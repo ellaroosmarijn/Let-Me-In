a65c456e60ee0d2dc6385f5cc51a96b1384c3d71
Ahmad Saleem <121984728+ahmad-saleem-23@users.noreply.github.com>
2023-04-06 15:05:31 +1200

    fixed db with scss

--- server/db/seeds/1-images.js
+++ server/db/seeds/1-images.js
@@ -2,7 +2,7 @@ exports.seed = async (knex) => {
   await knex('images').insert([
     {
       id: 1,
-      uploader_id: 'google-oauth2|104177402938278805058',
+      uploader_id: '1',
       name: 'Let Me In',
       description: 'LET ME INNNNNNNNNNNNNNN',
       image_url:
@@ -10,14 +10,14 @@ exports.seed = async (knex) => {
     },
     {
       id: 2,
-      uploader_id: 'google-oauth2|104177402938278805058',
+      uploader_id: '1',
       name: 'Judge Judy',
       description: 'Judge Judy time',
       image_url: 'https://media.tenor.com/vTY0qobiAtsAAAAC/judge-judy-time.gif',
     },
     {
       id: 3,
-      uploader_id: 'google-oauth2|104177402938278805058',
+      uploader_id: '2',
       name: 'One hour later',
       description: 'Two hours later, three hours later',
       image_url: '/images/hours-later-spongebob.gif',
